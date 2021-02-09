7e0aecc8fdb2de39c9adb32e69cc2e2baa2ad067
Nino-Black <76187762+Nino-Black@users.noreply.github.com>
2021-02-08 18:15:02 -0800

    unit 4

---
+++ Nino-Black apcsa/src/Unit4Assignments/Discount.java
@@ -0,0 +1,30 @@
+package Unit4Assignments;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - 
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class Discount
+{
+	//instance variables and constructors could be used, but are not really needed
+
+	//getDiscountedBill() will return final amount of the bill
+	//			if the bill is >2000, the bill receives a 15% discount
+	public static double getDiscountedBill(double bill)
+	{
+		if ( bill > 2000) {
+			double discount  = (bill * .15);
+			return bill - discount;
+		}
+		if ( bill <= 2000) {
+			return bill;
+		}
+		return 0;
+	}
+}
